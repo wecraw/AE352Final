d13e83e075a5d270d7258e8c595d136feac472dd
pdetrempe <pdetrempe@gmail.com>
2016-12-10 19:42:45 -0600

    Add files via upload
    includes working animated 3 plot. slight stutter on down swings. Has hella comments

--- code/pendulum_with_disk/AE352_Final_Project_Pendulum.m
+++ code/pendulum_with_disk/AE352_Final_Project_Pendulum.m
@@ -13,12 +13,16 @@ Numpeople=30;
 
 t0=0;
 theta0=.03;%radians
-thetadot0=0.1;%radians/second (give a kick to start the simulation
-phi0vec=linspace(0,2*pi,Numpeople)
+thetadot0=0.3;%radians/second (give a kick to start the simulation
+phi0vec=linspace(0,2*pi,Numpeople);%starting position of each rider
 phidot0=1;%radians/second
-tstep=0.05;
-for w=1:length(phi0vec)
-    phi0=phi0vec(w);
+tlength=50;%number of points for each numerical integration
+for w=1:length(phi0vec)%run through each 
+    theta0=.03;%radians
+    thetadot0=0.3;%radians/second (give a kick to start the simulation
+    phi0vec=linspace(0,2*pi,Numpeople)
+    phidot0=1;%radians/second
+    phi0=phi0vec(w);%current rider to work with
     t_out=[];
     y_out=[];
     %output vectors (appended on each run to show total run of pendulum)
@@ -29,14 +33,14 @@ for w=1:length(phi0vec)
 
         infovec=[theta0 thetadot0 phi0 phidot0];
         %tvec=t0:tstep:(T/2);%this for sure works
-        tvec=linspace(t0,T/2,50);
+        tvec=linspace(t0,T/2,tlength);
         [t, y]= ode45(@thetafunc, tvec,infovec);%go from t=0 to when pendulum completes...
         %half an oscillation (back at the bottom)
         t_out=t;
         y_out=y;
         tf=T/2;
         else
-        %load in new information
+        %load in new information from last ODE 45 run
         thetadot0=y(size(y,1),2);
         phi0=y(size(y,1),3);
         phidot0=y(size(y,1),4);
@@ -52,10 +56,11 @@ for w=1:length(phi0vec)
 
         infovec=[theta0 thetadotf phi0 phidot0];
         %tvec=tf:tstep:(tf+T/2);%this for sure works
-        tvec=linspace(tf,T/2+tf,50);
+        tvec=linspace(tf,T/2+tf,tlength);
         [t, y]= ode45(@thetafunc, tvec,infovec);%go from end of last run...
         %to when pendulum completes half an oscillation (back at the bottom)
         tf=tf+T/2;
+        %append outputs to outputs from previous runs to plot later
         t_out=[t_out;t];
         y_out=[y_out;y];
         end
@@ -85,20 +90,20 @@ for w=1:length(phi0vec)
         0 cos(thetaout(i)) -sin(thetaout(i));...
         0 sin(thetaout(i)) cos(thetaout(i))];
         P1=[r*cos(phiout(i)); r*sin(phiout(i)); 0];
-        Origin=[Origin,O_0to1];%store different origin values (used from plotting pendulum motion)
+        Origin=[Origin,O_0to1];%store different frame 1 origin values (used for plotting pendulum motion)
         P0=[P0,O_0to1+R0to1*P1];%store information about point in base frame
         %^above outputs x,y,z in rows 1, 2, 3
     end
     %initialize and store point vectors for each person
-    if w==1
+    if w==1%if first time, create storage arrays
         P0array=P0;
         OriginArray=Origin;
-    else
+    else%for each rider, store position and relative origin information
         P0array(:,:,w)=P0;
         OriginArray(:,:,w)=Origin;
     end
 end
-%plot outputs
+%plot theta output
 figure;
 plot(t_out,y_out(:,1));
 xlabel('Time, t, seconds')
@@ -110,21 +115,28 @@ figure;
 xlabel('x');
 ylabel('y');
 zlabel('z');
-for u=1:size(P0array,3)
-P0=P0array(:,:,u);
-Origin=OriginArray(:,:,u);
+
     for j=1:size(Origin,2)
-        personvector=[];
-        for count=1:size(P0array,3)
-        person=plot3(P0array(1,j,count),P0array(2,j,count),P0array(3,j,count),'ro');%point on disk
-        personvector=[personvector,person];
+        personvector=[];%used for storing points of people
+        %axis limits to make it look pretty
+        xlim([-25 25]);
+        ylim([-25 25]);
+        zlim([-25 25]);
+        grid on;
+        
+        for count=1:size(P0array,3)%run through each rider
+        %plot point on disk for each rider
+        person=plot3(P0array(1,j,count),P0array(2,j,count),P0array(3,j,count),'ro');
+        personvector=[personvector,person];%store plotted points for later deletion
         end
         hold on;
-        O=[0 0 0];
-        pendulum=plot3([0, Origin(1,j)],[0,Origin(2,j)],[0,Origin(3,j)],'g');%line for pendulum
-        pause(.1);
+        %plot a line from (0,0,0) to the frame 1 origin point. this is the
+        %pendulum
+        pendulum=plot3([0, Origin(1,j)],[0,Origin(2,j)],[0,Origin(3,j)],'g');
+        pause(.01);
         hold on;
+        %delete previous plotted points and pendulum
         delete(pendulum);  
         delete(personvector);
     end
-end
+% end
